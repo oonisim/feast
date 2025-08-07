fb0944d4995e99ef9bc7b1f870a604a3cb195578
oonisim <oonisim@gmail.com>
2025-08-07 17:30:34 +1000

    initial

--- my_project/feature_repo/example_repo.py
+++ my_project/feature_repo/example_repo.py
@@ -2,8 +2,6 @@
 
 from datetime import timedelta
 
-import pandas as pd
-
 from feast import (
     Entity,
     FeatureService,
@@ -11,20 +9,13 @@ from feast import (
     Field,
     FileSource,
     Project,
-    PushSource,
-    RequestSource,
 )
 from feast.feature_logging import LoggingConfig
 from feast.infra.offline_stores.file_source import FileLoggingDestination
-from feast.on_demand_feature_view import on_demand_feature_view
-from feast.types import Float32, Float64, Int64
+from feast.types import Float32, Int64
 
-# Define a project for the feature repo
 project = Project(name="my_project", description="A project for driver statistics")
 
-# Read data from parquet files. Parquet is convenient for local development mode. For
-# production, you can use your favorite DWH, such as BigQuery. See Feast documentation
-# for more info.
 driver_hourly_stats = FileSource(
     name="driver_hourly_stats_source",
     path="data/driver_stats.parquet",
@@ -32,17 +23,10 @@ driver_hourly_stats = FileSource(
     created_timestamp_column="created",
 )
 
-
-# Define an entity for the driver. You can think of an entity as a primary key used to
-# fetch features.
+# You can think of an entity as a primary key used to fetch features.
 driver = Entity(name="driver", join_keys=["driver_id"])
 
-# Our parquet files contain sample data that includes a driver_id column, timestamps and
-# three feature column. Here we define a Feature View that will allow us to serve this
-# data to our model online.
 driver_hourly_stats_view = FeatureView(
-    # The unique name of this feature view. Two feature views in a single
-    # project cannot have the same name
     name="driver_hourly_stats",
     entities=[driver],
     ttl=timedelta(days=1),
@@ -56,20 +40,15 @@ driver_hourly_stats_view = FeatureView(
     ],
     online=False,
     source=driver_hourly_stats,           # <--- Link to the raw data storage technology
-    # Tags are user defined key/value pairs that are attached to each
-    # feature view
     tags={"team": "driver_performance"},
 )
-
-# Define an on demand feature view which can generate new features based on
-# existing feature views and RequestSource features
 
 # This groups features into a model version
 driver_activity_v1 = FeatureService(
     name="driver_activity_v1",
     features=[
         # driver_hourly_stats_view[["conv_rate", "acc_rate"]],  # Sub-selects a feature from a feature view
-        driver_hourly_stats_view,  # Sub-selects a feature from a feature view
+        driver_hourly_stats_view,
     ],
     logging_config=LoggingConfig(
         destination=FileLoggingDestination(path="data")
